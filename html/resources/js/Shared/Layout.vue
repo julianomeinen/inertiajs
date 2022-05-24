2b408455e75f643a3ef569fb7e7fc9cfb9bdd888
Juliano Meinen <julianomeinen.souza@gmail.com>
2022-05-24 11:32:45 -0300

    Shared data with Inertia Laravel Middleware and Vue.

--- html/resources/js/Shared/Layout.vue
+++ html/resources/js/Shared/Layout.vue
@@ -1,13 +1,18 @@
 <template>
   <section class="p-6 bg-gray-200">
     <header class="flex justify-between">
-      <h1 class="font-bold text-lg">App Name</h1>
+      <div class="flex items-center">
+        <h1 class="font-bold text-lg">App Name</h1>
+        <p class="text-sm ml-4">
+          Welcome back, {{ username }}
+        </p>
+      </div>
       <Nav />
     </header>
   </section>
   <section class="p-6">
     <div class="max-w-3xl mx-auto">
-        <slot />
+      <slot />
     </div>
   </section>
 </template>
@@ -17,5 +22,11 @@ import Nav from "./Nav.vue";
 
 export default {
   components: { Nav },
+
+  computed: {
+      username() {
+          return this.$page.props.auth.user.username
+      }
+  }
 };
 </script>
