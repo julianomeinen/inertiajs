7c471028f5e2d36913c123cdcfee69194d96ca1f
Juliano Meinen <julianomeinen.souza@gmail.com>
2022-05-24 11:18:21 -0300

    Shared layout for vue app.

--- html/resources/js/Pages/Users.vue
+++ html/resources/js/Pages/Users.vue
@@ -1,18 +1,31 @@
 <template>
-  <h1>Users</h1>
-  <Nav />
-  <div class="mt-80 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-    <p>The current time is {{ time }}.</p>
-    <Link href="/users" class="text-blue-500" preserve-scroll >Refresh</Link>
-  </div>
+  <Layout>
+    <h1 class="text-3xl">Users</h1>
+    <div
+      class="
+        mt-80
+        py-8
+        px-8
+        max-w-sm
+        bg-white
+        rounded-xl
+        shadow-lg
+        space-y-2
+        sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6
+      "
+    >
+      <p>The current time is {{ time }}.</p>
+      <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
+    </div>
+  </Layout>
 </template>
 
 <script>
 import { Link } from '@inertiajs/inertia-vue3';
-import Nav from "../Shared/Nav";
+import Layout from '../Shared/Layout';
 
 export default {
-  components: { Nav, Link },
+  components: { Layout, Link },
   props: {
     time: String,
   },
